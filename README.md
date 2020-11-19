6cf9d24a51bde98a10c461d37fc38bffa0ace37e
alexleung <alexchleung@astri.org>
2020-11-19 16:52:47 +0800

    grid search

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MSBD5001GroupProject
+
+# XGBoost
+before grid search :
+ - public score : 0.935815
+ - private score : 0.901756
+
+after grid search:
+ - public score : 0.937161
+ - private score : 0.905312
+
--- src/xgboostBaseline.py
+++ src/xgboostBaseline.py
@@ -1,11 +1,13 @@
 # Reference : https://www.kaggle.com/xhlulu/ieee-fraud-xgboost-with-gpu-fit-in-40s
 
 import os
+import time
 import pdb
 
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
+from sklearn.model_selection import GridSearchCV
 import xgboost as xgb
 
 train_transaction = pd.read_csv('../input/train_transaction.csv', index_col='TransactionID')
@@ -47,6 +49,16 @@ for f in X_train.columns:
         X_train[f] = lbl.transform(list(X_train[f].values))
         X_test[f] = lbl.transform(list(X_test[f].values))
 
+parameters = {
+    'max_depth': [9, 12, 15],
+    'learning_rate': [0.02, 0.05, 0.1,],
+    'n_estimators': [500, 1000, 2000],
+    'subsample': [0.8, 0.85, 0.9],
+    'colsample_bytree': [0.7, 0.8, 0.9],
+    'missing': [-999],
+    'tree_method': ['gpu_hist']
+}
+
 clf = xgb.XGBClassifier(
     n_estimators=500,
     max_depth=9,
@@ -58,6 +70,32 @@ clf = xgb.XGBClassifier(
     tree_method='gpu_hist'  # THE MAGICAL PARAMETER
 )
 
-clf.fit(X_train, y_train)
-sample_submission['isFraud'] = clf.predict_proba(X_test)[:,1]
+clf_best_param = xgb.XGBClassifier( # Best score: 0.96864
+    n_estimators=500,
+    max_depth=15,
+    learning_rate=0.02,
+    subsample=0.85,
+    colsample_bytree=0.7,
+    missing=-999,
+    random_state=2019,
+    tree_method='gpu_hist'  # THE MAGICAL PARAMETER
+)
+
+start_t = time.time()
+'''
+# gsearch = GridSearchCV(clf, param_grid=parameters, cv=3, n_jobs=4) # it runs several hours
+# gsearch.fit(X_train, y_train)
+
+# print("Best score: %0.5f" % gsearch.best_score_)
+# print("Best parameters set:")
+# best_parameters = gsearch.best_estimator_.get_params()
+# for param_name in sorted(parameters.keys()):
+#     print("\t%s: %r" % (param_name, best_parameters[param_name]))
+'''
+
+clf_best_param.fit(X_train, y_train)
+sample_submission['isFraud'] = clf_best_param.predict_proba(X_test)[:,1]
 sample_submission.to_csv('simple_xgboost.csv')
+
+end_t = time.time()
+print('Running time =', end_t - start_t)
