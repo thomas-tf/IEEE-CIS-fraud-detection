15b785979f06d63a72fad7254351940f2aa6e303
royinx <royfork@hotmail.com>
2020-11-21 21:57:38 +0800

    lightGBM + GCN placeholder

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,11 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     rm -rf /var/lib/apt/lists/*
 COPY requirement.txt .
 RUN pip3 install --upgrade pip && \
-    pip3 install -r requirement.txt
+    pip3 install -r requirement.txt && \
+    export CUDA=cu110 TORCH=1.7.0 && \
+    pip install torch==${TORCH}+${CUDA} torchvision==0.8.1+${CUDA} torchaudio===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html && \
+    pip install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html && \
+    pip install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html && \
+    pip install torch-cluster==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html && \
+    pip install torch-spline-conv==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html && \
+    pip install torch-geometric
---
+++ src/recursive_feature_elimiation.py
@@ -0,0 +1,133 @@
+import gc
+import numpy as np
+
+from sklearn.model_selection import KFold
+from sklearn.feature_selection import RFECV
+from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder
+
+
+def reduce_mem_usage(df, verbose=True):
+    """
+    reduce memory usage by casting datatype to lower bits
+    :param df:
+    :param verbose:
+    :return df:
+    """
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+    for col in df.columns:
+
+        col_type = df[col].dtypes
+
+        if col_type in numerics:
+
+            c_min = df[col].min()
+            c_max = df[col].max()
+
+            if str(col_type)[:3] == 'int':
+
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+
+            else:
+
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+
+    if verbose:
+        print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(
+            end_mem, 100 * (start_mem - end_mem) / start_mem)
+        )
+    return df
+
+
+def label_encoding(df):
+    for col in tqdm(df.columns, desc="Label Encoding..."):
+        if df[col].dtype == 'object':
+            le = LabelEncoder()
+            le.fit(list(df[col].astype(str).values) + list(df[col].astype(str).values))
+            df[col] = le.transform(list(df[col].astype(str).values))
+
+
+def recursive_feature_elimination(train, from_backup=True):
+    """
+    conduct recursive feature elimination on the given training dataset
+    :param train: training dataset
+    :param from_backup: load from historical result (stored as list of strings), defaults to True
+    :return top ranked features:
+    """
+    # defaults to return backup list
+    if from_backup:
+        return ['TransactionAmt', 'ProductCD', 'card1', 'card2', 'card3', 'card4', 'card5', 'card6', 'addr1', 'addr2',
+                'dist1', 'P_emaildomain', 'R_emaildomain', 'C1', 'C2', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11',
+                'C12', 'C13', 'C14', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D8', 'D9', 'D10', 'D11', 'D12', 'D13', 'D14',
+                'D15', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9', 'V3', 'V4', 'V5', 'V7', 'V12', 'V13', 'V19',
+                'V20', 'V29', 'V30', 'V33', 'V34', 'V35', 'V36', 'V37', 'V38', 'V40', 'V43', 'V44', 'V45', 'V47', 'V48',
+                'V49', 'V51', 'V52', 'V53', 'V54', 'V56', 'V57', 'V58', 'V60', 'V61', 'V62', 'V69', 'V70', 'V72', 'V74',
+                'V75', 'V76', 'V78', 'V81', 'V82', 'V83', 'V87', 'V90', 'V91', 'V94', 'V95', 'V96', 'V97', 'V99',
+                'V100', 'V126', 'V127', 'V128', 'V130', 'V131', 'V139', 'V140', 'V143', 'V145', 'V149', 'V150', 'V152',
+                'V156', 'V158', 'V159', 'V160', 'V162', 'V164', 'V165', 'V166', 'V167', 'V169', 'V170', 'V171', 'V187',
+                'V188', 'V189', 'V200', 'V201', 'V202', 'V203', 'V204', 'V205', 'V206', 'V207', 'V208', 'V209', 'V210',
+                'V212', 'V213', 'V215', 'V216', 'V217', 'V218', 'V219', 'V221', 'V222', 'V223', 'V224', 'V225', 'V226',
+                'V228', 'V231', 'V232', 'V233', 'V234', 'V243', 'V244', 'V251', 'V254', 'V256', 'V257', 'V258', 'V261',
+                'V262', 'V263', 'V264', 'V265', 'V266', 'V267', 'V268', 'V270', 'V271', 'V272', 'V273', 'V274', 'V275',
+                'V276', 'V277', 'V278', 'V279', 'V280', 'V282', 'V283', 'V285', 'V287', 'V288', 'V289', 'V291', 'V292',
+                'V294', 'V303', 'V306', 'V307', 'V308', 'V310', 'V312', 'V313', 'V314', 'V315', 'V317', 'V322', 'V323',
+                'V324', 'V326', 'V331', 'V332', 'V333', 'V335', 'id_01', 'id_02', 'id_03', 'id_05', 'id_06', 'id_09',
+                'id_13', 'id_14', 'id_15', 'id_16', 'id_17', 'id_19', 'id_20', 'id_30', 'id_31', 'id_32', 'id_33',
+                'id_38', 'DeviceType', 'DeviceInfo', 'device_name', 'OS_id_30', 'version_id_30', 'browser_id_31',
+                'version_id_31', 'screen_width', 'screen_height', 'P_emaildomain_bin', 'P_emaildomain_suffix',
+                'R_emaildomain_bin', 'R_emaildomain_suffix', 'TransactionAmt_Log', 'TransactionAmt_decimal']
+
+        # if desire to conduct RFE again...
+    train = reduce_mem_usage(train)
+
+    X = train.sort_values('TransactionDT').drop(['isFraud', 'TransactionDT'], axis=1)
+    y = train.sort_values('TransactionDT')['isFraud']
+
+    del train
+    gc.collect()
+
+    X.fillna(-999, inplace=True)
+
+    # parameters chosen by BayesianOptimization
+    # Credit to this notebook: https://www.kaggle.com/vincentlugat/ieee-lgb-bayesian-opt/notebook
+    params = {
+        'num_leaves': 491,
+        'min_child_weight': 0.03454472573214212,
+        'feature_fraction': 0.3797454081646243,
+        'bagging_fraction': 0.4181193142567742,
+        'min_data_in_leaf': 106,
+        'objective': 'binary',
+        'max_depth': -1,
+        'learning_rate': 0.006883242363721497,
+        "boosting_type": "gbdt",
+        "bagging_seed": 11,
+        "metric": 'auc',
+        "verbosity": -1,
+        'reg_alpha': 0.3899927210061127,
+        'reg_lambda': 0.6485237330340494,
+        'random_state': 47
+    }
+
+    import lightgbm as lgb
+
+    clf = lgb.LGBMClassifier(**params)
+    rfe = RFECV(estimator=clf, step=10, cv=KFold(n_splits=5, shuffle=False), scoring='roc_auc', verbose=2)
+
+    rfe.fit(X, y)
+
+    return X.columns[rfe.ranking_ == 1].tolist()
